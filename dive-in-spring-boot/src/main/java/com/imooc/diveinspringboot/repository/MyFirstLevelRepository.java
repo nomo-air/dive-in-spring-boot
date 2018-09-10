203542fc2aa36618dc07c9dff701eff5c6d21e2e
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-10 10:59:07 +0800

    feat.@Component派生性

---
+++ dive-in-spring-boot/src/main/java/com/imooc/diveinspringboot/repository/MyFirstLevelRepository.java
@@ -0,0 +1,8 @@
+package com.imooc.diveinspringboot.repository;
+
+import com.imooc.diveinspringboot.annotation.FirstLevelRepository;
+
+@FirstLevelRepository(value = "myFirstLevelRepository")
+public class MyFirstLevelRepository {
+
+}
