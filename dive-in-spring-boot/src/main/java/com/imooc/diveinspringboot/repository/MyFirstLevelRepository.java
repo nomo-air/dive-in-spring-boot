43359ca975aa4566223201a7453ceab0acf1cd6d
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-10 11:06:21 +0800

    feat.@Component层次性

--- dive-in-spring-boot/src/main/java/com/imooc/diveinspringboot/repository/MyFirstLevelRepository.java
+++ dive-in-spring-boot/src/main/java/com/imooc/diveinspringboot/repository/MyFirstLevelRepository.java
@@ -1,8 +1,9 @@
 package com.imooc.diveinspringboot.repository;
 
-import com.imooc.diveinspringboot.annotation.FirstLevelRepository;
+import com.imooc.diveinspringboot.annotation.SecondLevelRepository;
 
-@FirstLevelRepository(value = "myFirstLevelRepository")
+// @FirstLevelRepository(value = "myFirstLevelRepository")
+@SecondLevelRepository(value = "myFirstLevelRepository")
 public class MyFirstLevelRepository {
 
 }
