fb7116ef8c9b64dafedf53f5c57e0760e49fccf2
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-13 21:36:21 +0800

    feat.web mvc 注解模式

---
+++ spring-web-mvc/src/main/java/com/imooc/web/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.imooc.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring Web MVC 配置
+ */
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+}
