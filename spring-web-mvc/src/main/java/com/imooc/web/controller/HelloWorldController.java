872655e060556e0a675e81c1b2332725d3c08b85
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-13 21:16:31 +0800

    feat.spring web mvc

---
+++ spring-web-mvc/src/main/java/com/imooc/web/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.imooc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("")
+    public String index() {
+        return "index";
+    }
+}
