ebb57b680da5ad00d6fe28a714930b10f51738f7
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-14 11:16:43 +0800

    feat.Web MVC 中 @ControllerAdvice，@ExceptionHandler

--- spring-web-mvc/src/main/java/com/imooc/web/controller/HelloWorldController.java
+++ spring-web-mvc/src/main/java/com/imooc/web/controller/HelloWorldController.java
@@ -6,32 +6,19 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping("")
     public String index(
-                      //  @RequestHeader("Accept-Language") String acceptLanguage,
-                      //  @CookieValue("JSESSIONID") String jsessionId,
-                        Model model) {
+            //  @RequestHeader("Accept-Language") String acceptLanguage,
+            //  @CookieValue("JSESSIONID") String jsessionId,
+            @RequestParam int value, Model model) {
         // model.addAttribute("acceptLanguage", acceptLanguage);
         // model.addAttribute("jsessionId", jsessionId);
         // model.addAttribute("message", "Hello World");
         return "index";
-    }
-    @ModelAttribute("acceptLanguage")
-    public String acceptLanguage(@RequestHeader("Accept-Language") String acceptLanguage) {
-        return acceptLanguage;
-    }
-
-    @ModelAttribute("jsessionId")
-    public String jsessionId(@CookieValue("JSESSIONID") String jsessionId) {
-        return jsessionId;
-    }
-
-    @ModelAttribute("message")
-    public String message() {
-        return "Hello World";
     }
 }
