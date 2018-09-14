3b2cc434118568bb0edebf865f997e7b561ceeb4
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-14 16:16:58 +0800

    fix.重构Springframework-webmvc

---
+++ springboot-webmvc/src/main/java/com/imooc/web/bootstrap/SpringBootWebMvcBootstrap.java
@@ -0,0 +1,13 @@
+package com.imooc.web.bootstrap;
+
+        import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Spring Boot Web MVC 引导类
+ */
+@ComponentScan()
+public class SpringBootWebMvcBootstrap {
+    public static void main(String[] args) {
+
+    }
+}
