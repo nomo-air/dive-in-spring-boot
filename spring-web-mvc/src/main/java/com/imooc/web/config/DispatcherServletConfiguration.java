26669992eb5773c95acfc737b6cf777b67b7947f
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-14 13:07:33 +0800

    feat.Web MVC 自动装配实现

---
+++ spring-web-mvc/src/main/java/com/imooc/web/config/DispatcherServletConfiguration.java
@@ -0,0 +1,12 @@
+package com.imooc.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * {@link DispatcherServlet} 配置类
+ */
+@ComponentScan(basePackages = "com.imooc.web")
+public class DispatcherServletConfiguration {
+
+}
