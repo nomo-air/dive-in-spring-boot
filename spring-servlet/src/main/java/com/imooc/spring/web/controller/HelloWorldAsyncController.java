e2767d8afc809f07f47e527b8f2edcba3c278975
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-30 13:31:02 +0800

    feat.Callable 异步执行

--- spring-servlet/src/main/java/com/imooc/spring/web/controller/HelloWorldAsyncController.java
+++ spring-servlet/src/main/java/com/imooc/spring/web/controller/HelloWorldAsyncController.java
@@ -9,6 +9,7 @@ import org.springframework.web.context.request.async.DeferredResult;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
 
 
 /**
@@ -38,6 +39,17 @@ public class HelloWorldAsyncController {
             result.setResult("Hello World");
             println("执行计算结果，消耗" + timeout + " ms. ");
         } while (result != null);
+    }
+
+    @GetMapping("/callable-hello-world")
+    public Callable<String> callableHelloWorld() {
+        final long startTime = System.currentTimeMillis();
+        println("Hello World");
+        return () -> {
+            long costTime = System.currentTimeMillis() - startTime;
+            println("执行计算结果，消耗" + costTime + " ms. ");
+            return "Hello World";
+        };
     }
 
 
