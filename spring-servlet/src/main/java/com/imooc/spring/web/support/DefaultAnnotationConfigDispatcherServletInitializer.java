3cdd2a835f56c00bfbc528cddbe72d4a0a80e0f5
zhengwanlei <zhengwanlei@nibirutech.com>
2018-09-30 13:23:58 +0800

    feat.DeferredResult 异步执行

---
+++ spring-servlet/src/main/java/com/imooc/spring/web/support/DefaultAnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,28 @@
+package com.imooc.spring.web.support;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Spring Web MVC 自动装配 默认实现
+ */
+@ComponentScan(basePackages = "com.imooc.spring.web.controller")
+// @Configuration 可以不加
+public class DefaultAnnotationConfigDispatcherServletInitializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() { // web.xml
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() { // DispatcherServlet
+        return new Class[]{getClass()}; // 返回当前类
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
